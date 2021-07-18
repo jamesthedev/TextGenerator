6f3b00643c36c5ecf72e0d40e73b385c81b9f68e
James <james989@outlook.com>
2021-07-18 18:43:25 -0500

    added comment to genLoremIpsum

--- clientScript.js
+++ clientScript.js
@@ -105,6 +105,7 @@ function genRandomWords(count) {
     });
 }
 
+//just repeats the basic lorem ipsum paragraph
 function genLoremIpsum(count) {
     var liText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     var liArray = liText.split(" "); //because I'm lazy
